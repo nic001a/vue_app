3f47eb5b9510cbcfdec06a0027435724d3c68fb3
nic001a <nicola.info.dimarco@gmail.com>
2022-02-15 16:25:15 +0100

    in progress

--- weather/src/App.vue
+++ weather/src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data (){
+    return {
+      api_key : '528141e5d9f4386c3a6f3fef6cb3a718'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ 
 </style>
